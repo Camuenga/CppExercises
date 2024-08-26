e87cc619121473635e45d16a4a606cf58c7e424b
CamuengaPortifolio <167720838+Camuenga@users.noreply.github.com>
2024-08-26 10:13:27 +0100

    Add files via upload

---
+++ structs/1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+	struct horario{
+		int hour{},minute{},second{};
+	};
+	struct date{
+		int day{},month{},year{};
+	};
+	struct comprimisse{
+		date date_comprisse{};
+		horario horario_comprimesse{};
+		std::string text_comprimesse{""};
+	};
+
+int main(){
+	
+
+	return 0;
+}
